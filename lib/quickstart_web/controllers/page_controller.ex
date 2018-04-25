4954f2c5261f135b0be6c904516f4cc3a89867ba
Feihong Hsu <feihong.hsu@gmail.com>
2018-04-24 22:00:09 -0500

    Show random hanzi

--- lib/quickstart_web/controllers/page_controller.ex
+++ lib/quickstart_web/controllers/page_controller.ex
@@ -1,7 +1,12 @@
 defmodule QuickstartWeb.PageController do
   use QuickstartWeb, :controller
 
-  def index(conn, _params) do
-    render conn, "index.html"
+  def index(conn, params) do
+    # By default, show 3 hanzi
+    # Todo: better error handling
+    count = Map.get(params, "count", "3")
+            |> Integer.parse
+            |> elem(0)
+    render conn, "index.html", count: count
   end
 end
--- lib/quickstart_web/views/page_view.ex
+++ lib/quickstart_web/views/page_view.ex
@@ -1,3 +1,11 @@
 defmodule QuickstartWeb.PageView do
   use QuickstartWeb, :view
+
+  def random_hanzi(count) do
+    # Return sequence of `count` random hanzi characters
+    for _ <- 1..count do
+      ordinal = Enum.random(0x4e00..0x9fff)
+      List.to_string([ordinal])
+    end
+  end
 end
